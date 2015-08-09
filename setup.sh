63ae8d39a284d2a045858a2e19a66930b532f2aa
kevin <kevinkim1991@gmail.com>
2015-08-08 20:06:04 -0400

    fixed script

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ cp -r ftplugin ~/.vim/
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
-git clone https://github.com/tpop/vim-vinegar.git
+git clone https://github.com/tpope/vim-vinegar
